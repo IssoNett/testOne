86ee8e8d91b98ec470ee8030a541c4abe6a2f304
fimbo <fimbo@nfinic.com>
2019-10-01 15:15:24 +0300

    first commit

---
+++ venv/main.py
@@ -0,0 +1,24 @@
+
+# def myfunc():
+#   global x
+#   x = "fantastic"
+#   print(x)
+#
+# myfunc()
+#
+# x = ["apple", "banana", "cherry"]
+# x = dict(name="John", age=36)
+#
+# print(x["name"])
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  #print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+
+
